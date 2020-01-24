fcad2616acefc7605c3c31959d573cca6f9fa5ed
Marco <marcopa84@hotmail.it>
2020-01-24 16:53:38 +0100

    FUNZIONE CANCELLA MESSAGGIO OK!

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 
 //MESSAGGI
+// invia messaggio
 $('#send').click(
   function () {
     send ();
   }
 );
-
 
 function send() {
   var message_baloon = $('.template-chat .chat-message').clone();
@@ -25,28 +25,28 @@ function reply() {
   $('.chat-window').append(message_baloon);
 }
 
-
+// cancella messaggio
+// --aprire finestra
+$('.chat-message').click(
+  function () {
+    $('.fa-chevron-down').removeClass('fa-chevron-down').addClass('fa-chevron-up');
+    $('.chat-command-window').show();
+  }
+);
+// --cancellare messaggio
+$('.chat-command-window').children('p').click(
+  function () {
+    $('.chat-command-window').parent('.chat-command').parent('.chat-message').remove();
+  }
+);
+// --chiudere finestra
+$('.chat-message').mouseleave(
+  function () {
+    $('.fa-chevron-up').removeClass('fa-chevron-up').addClass('fa-chevron-down');
+    $('.chat-command-window').hide();
+  }
+);
 // CONTATTI
-// $('#ico_search').click(
-//   function () {
-//     var search_input = $('#input_search').val();
-//     console.log(search_input);
-//     var contact_row = $('#list_contact').children('li');
-//     console.log(contact_row);
-//
-//     for (var i = 0; i < contact_row.length; i++) {
-//       var contact_name = $('#list_contact').children('li').children('.contact-info').children('p.text-name').eq(i).text();
-//       console.log(contact_name);
-//       if (contact_name.includes(search_input)) {
-//         contact_row.eq(i).show();
-//         console.log('uguale');
-//       }else {
-//         contact_row.eq(i).hide();
-//         console.log('diverso');
-//       }
-//     }
-//   }
-// );
 
 $('#ico_search').click(
   function  () {
